17c3d3022572024085003581654e7cb6f3c3ad11
lml19960131 <lml19960131@126.com>
2017-12-12 14:12:11 +0800

    Layout

---
+++ src/main.js
@@ -0,0 +1,25 @@
+import Vue from 'vue'
+import Layout from './components/layout.vue'
+import VueRouter from 'vue-router'
+import VueResource from 'vue-resource'
+import IndexPage from './pages/index.vue'
+
+Vue.use(VueRouter);
+Vue.use(VueResource);
+
+let router = new VueRouter({
+  mode: 'history',
+  routes: [
+    {
+      path: '/',
+      component: IndexPage
+    },
+  ]
+})
+
+new Vue({
+  el: '#app',
+  router,
+  template: '<Layout/>',
+  components: { Layout }
+});
