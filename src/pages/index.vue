b9f0ea45acff938f7c43b124a0984f18e85ee9d9
lml19960131 <lml19960131@126.com>
2017-12-12 18:45:44 +0800

    slide

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -24,6 +24,7 @@
       </div>
     </div>
     <div class="index-right">
+      <slide-show :slides="slides" :inv="invTime"></slide-show>
       <div class="index-board-list">
         <div class="index-board-item"
              v-for="(item, index) in boardList"
@@ -42,15 +43,20 @@
 </template>
 
 <script>
+  import slideShow from '../components/baseComponents/slideShow.vue'
+
   export default{
-    created:function (){
-      this.$http.get('getNewsList')
-        .then(function (data) {
-          console.log(data)
-        },function (err) {
-          console.log(err)
-        })
+    components:{
+      slideShow
     },
+//    created:function (){
+//      this.$http.get('getNewsList')
+//        .then(function (data) {
+//          console.log(data)
+//        },function (err) {
+//          console.log(err)
+//        })
+//    },
     data(){
       return{
         productList: {
@@ -116,6 +122,29 @@
           {
             title: '广告发布',
             url: 'http://hearstone.com'
+          }
+        ],
+        invTime: 2000,
+        slides: [
+          {
+            src: require('../assets/slideShow/pic1.jpg'),
+            title: 'xxx1',
+            href: 'detail/analysis'
+          },
+          {
+            src: require('../assets/slideShow/pic2.jpg'),
+            title: 'xxx2',
+            href: 'detail/count'
+          },
+          {
+            src: require('../assets/slideShow/pic3.jpg'),
+            title: 'xxx3',
+            href: 'http://xxx.xxx.com'
+          },
+          {
+            src: require('../assets/slideShow/pic4.jpg'),
+            title: 'xxx4',
+            href: 'detail/forecast'
           }
         ],
         boardList: [
